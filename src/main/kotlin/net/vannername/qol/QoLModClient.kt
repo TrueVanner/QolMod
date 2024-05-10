4a212af3935e659e3e13f89faf016057e3a702bd
TrueVanner <vannername@gmail.com>
2024-05-10 11:33:36 +0200

    Fucking with rendering 4: i think i'm getting somewhere
    Took 35 minutes

--- src/main/kotlin/net/vannername/qol/QoLModClient.kt
+++ src/main/kotlin/net/vannername/qol/QoLModClient.kt
@@ -2,12 +2,16 @@ package net.vannername.qol
 
 import com.mojang.blaze3d.systems.RenderSystem
 import me.x150.renderer.event.RenderEvents
+import me.x150.renderer.render.MSAAFramebuffer
 import me.x150.renderer.render.Renderer2d
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.ServerStarted
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.network.ClientPlayerEntity
 import net.minecraft.client.render.*
 import net.minecraft.client.render.model.json.ModelTransformationMode
+import net.minecraft.client.session.telemetry.WorldLoadedEvent
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.component.DataComponentTypes
 import net.minecraft.component.type.LodestoneTrackerComponent
@@ -23,16 +27,28 @@ object QoLModClient : ClientModInitializer {
 
 	override fun onInitializeClient() {
 
-		RenderEvents.HUD.register { matrices ->
-//			RendererUtils.registerBufferedImageTexture(Identifier("nav_compass"), ImageIO.read(File("C:/Users/the_best/Documents/BlockBench Models/nav compass/compass_00.png")))
+		ServerLifecycleEvents.SERVER_STARTED.register { server ->
 
-			renderCompass(matrices.matrices, MinecraftClient.getInstance().player!!)
-		//			ClientTickEvents.END_WORLD_TICK.register { world ->
-//				for(p in world.players) {
-//
-//				}
-//			}
+			val client = MinecraftClient.getInstance()
+			val compass = ItemStack(Items.COMPASS)
+			client.textureManager.getTexture(Identifier("renderer", "icon.png"))
+			println(client.textureManager.getTexture(Identifier("compass_00")))
 		}
+
+		RenderEvents.HUD.register { matrices ->
+
+		}
+
+//		RenderEvents.HUD.register { matrices ->
+////			RendererUtils.registerBufferedImageTexture(Identifier("nav_compass"), ImageIO.read(File("C:/Users/the_best/Documents/BlockBench Models/nav compass/compass_00.png")))
+//
+//			renderCompass(matrices.matrices, MinecraftClient.getInstance().player!!)
+//		//			ClientTickEvents.END_WORLD_TICK.register { world ->
+////				for(p in world.players) {
+////
+////				}
+////			}
+//		}
 
 	}
 
@@ -41,27 +57,32 @@ object QoLModClient : ClientModInitializer {
 //			RenderSystem.setShaderTexture(0, Identifier("minecraft:item/compass_00"))
 //		Renderer2d.renderTexture(matrixStack, Identifier("nav_compass"), 100.0, 100.0, 16.0, 16.0)
 
+		Renderer2d.renderTexture(matrixStack, Identifier("compass"), 100.0, 100.0, 16.0, 16.0)
+
 //		p.sendMessage(Text.literal(CompassAnglePredicateProvider { world, stack, entity -> GlobalPos(world.registryKey, BlockPos(0, 50, 0)) }
 //			.unclampedCall(
 //				ItemStack(Items.COMPASS), p.clientWorld, p, 0
 //			).toString()))
 
-//		Renderer2d.renderTexture(matrixStack, )
 
 		val client = MinecraftClient.getInstance()
 		val compass = ItemStack(Items.COMPASS)
 		compass.set(DataComponentTypes.LODESTONE_TRACKER, LodestoneTrackerComponent(Optional.of(GlobalPos.create(p.world.registryKey, BlockPos(0, 50, 0))), true))
+		client.textureManager.getTexture(Identifier("item", "compass.json"))
+		client.textureManager.getTexture(Identifier("compass_00"))
 
-		try {
-			val tessellator = Tessellator.getInstance()
-			val builder = tessellator.buffer
-//			client.itemRenderer.renderItem(compass, ModelTransformationMode.GROUND, 0xF000F0, OverlayTexture.DEFAULT_UV, matrixStack, VertexConsumerProvider.immediate(builder), null, 0)
-			client.itemRenderer.renderItem(ItemStack(Items.DIRT), ModelTransformationMode.GROUND, 0xF000F0, OverlayTexture.DEFAULT_UV, matrixStack, VertexConsumerProvider.immediate(builder), null, 0)
-			RenderSystem.setShader { GameRenderer.getPositionTexProgram() }
-			tessellator.draw()
-		} catch (e: Exception) {
-			println("fuckup")
-		}
+	//		println(compass.registryEntry.key)
+//		try {
+//			val tessellator = Tessellator.getInstance()
+//			val builder = tessellator.buffer
+//
+////			client.itemRenderer.renderItem(compass, ModelTransformationMode.GROUND, 0xF000F0, OverlayTexture.DEFAULT_UV, matrixStack, VertexConsumerProvider.immediate(builder), null, 0)
+////			client.itemRenderer.renderItem(ItemStack(Items.DIRT), ModelTransformationMode.GROUND, 0xF000F0, OverlayTexture.DEFAULT_UV, matrixStack, VertexConsumerProvider.immediate(builder), null, 0)
+////			RenderSystem.setShader { GameRenderer.getPositionTexProgram() }
+//			tessellator.draw()
+//		} catch (e: Exception) {
+//			println("fuckup")
+//		}
 
 
 //		for(quad in MinecraftClient.getInstance().itemRenderer.getModel(compass, null, null, 0).getQuads(null, null, Random.create())) {
