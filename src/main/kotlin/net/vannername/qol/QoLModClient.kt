d8cef9af0dbf8dd528cc941aff70684ef9709325
TrueVanner <vannername@gmail.com>
2024-05-10 12:02:35 +0200

    Fucking with rendering 6: it works! it's bad but it works
    Took 19 minutes

--- src/main/kotlin/net/vannername/qol/QoLModClient.kt
+++ src/main/kotlin/net/vannername/qol/QoLModClient.kt
@@ -1,57 +1,72 @@
 package net.vannername.qol
 
-import com.mojang.blaze3d.systems.RenderSystem
 import me.x150.renderer.event.RenderEvents
-import me.x150.renderer.render.MSAAFramebuffer
 import me.x150.renderer.render.Renderer2d
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.ServerStarted
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.item.CompassAnglePredicateProvider
 import net.minecraft.client.network.ClientPlayerEntity
-import net.minecraft.client.render.*
-import net.minecraft.client.render.model.json.ModelTransformationMode
-import net.minecraft.client.session.telemetry.WorldLoadedEvent
 import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.component.DataComponentTypes
-import net.minecraft.component.type.LodestoneTrackerComponent
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.GlobalPos
-import org.lwjgl.BufferUtils
-import java.util.*
 
 object QoLModClient : ClientModInitializer {
 
 	override fun onInitializeClient() {
-
-		ServerLifecycleEvents.SERVER_STARTED.register { server ->
-
-			val client = MinecraftClient.getInstance()
-			val compass = ItemStack(Items.COMPASS)
-//			client.textureManager.getTexture()
-//			println(client.textureManager.getTexture(Identifier("compass_00")))
-		}
-
+		val angleProvider = CompassAnglePredicateProvider { world, _, _ -> GlobalPos(world.registryKey, BlockPos(0, 50, 0)) }
 		RenderEvents.HUD.register { matrices ->
-			renderCompass(matrices.matrices, MinecraftClient.getInstance().player!!)
+			renderCompass(matrices.matrices, angleProvider, MinecraftClient.getInstance().player!!)
 		}
 
 	}
 
 
-	private fun renderCompass(matrixStack: MatrixStack, p: ClientPlayerEntity) {
-//			RenderSystem.setShaderTexture(0, Identifier("minecraft:item/compass_00"))
-//		Renderer2d.renderTexture(matrixStack, Identifier("nav_compass"), 100.0, 100.0, 16.0, 16.0)
+	private fun renderCompass(matrixStack: MatrixStack, angleProvider: CompassAnglePredicateProvider, p: ClientPlayerEntity) {
+		val angle = (angleProvider.unclampedCall(
+			ItemStack(Items.COMPASS), p.clientWorld, p, 0
+		))
 
-		Renderer2d.renderTexture(matrixStack, Identifier("textures/item/compass_00.png"), 100.0, 100.0, 16.0, 16.0)
+		val textureID: String = when {
+			angle == 0f -> "16"
+			angle < 0.015625f -> "17"
+			angle < 0.046875f -> "18"
+			angle < 0.078125f -> "19"
+			angle < 0.109375f -> "20"
+			angle < 0.140625f -> "21"
+			angle < 0.171875f -> "22"
+			angle < 0.203125f -> "23"
+			angle < 0.234375f -> "24"
+			angle < 0.265625f -> "25"
+			angle < 0.296875f -> "26"
+			angle < 0.328125f -> "27"
+			angle < 0.359375f -> "28"
+			angle < 0.390625f -> "29"
+			angle < 0.421875f -> "30"
+			angle < 0.453125f -> "31"
+			angle < 0.484375f -> "00"
+			angle < 0.515625f -> "01"
+			angle < 0.546875f -> "02"
+			angle < 0.578125f -> "03"
+			angle < 0.609375f -> "04"
+			angle < 0.640625f -> "05"
+			angle < 0.671875f -> "06"
+			angle < 0.703125f -> "07"
+			angle < 0.734375f -> "08"
+			angle < 0.765625f -> "09"
+			angle < 0.796875f -> "10"
+			angle < 0.828125f -> "11"
+			angle < 0.859375f -> "12"
+			angle < 0.890625f -> "13"
+			angle < 0.921875f -> "14"
+			angle < 0.953125f -> "15"
+			else -> "16"
+		}
 
-//		p.sendMessage(Text.literal(CompassAnglePredicateProvider { world, stack, entity -> GlobalPos(world.registryKey, BlockPos(0, 50, 0)) }
-//			.unclampedCall(
-//				ItemStack(Items.COMPASS), p.clientWorld, p, 0
-//			).toString()))
+		Renderer2d.renderTexture(matrixStack, Identifier("textures/item/compass_${textureID}.png"), 100.0, 100.0, 16.0, 16.0)
+
 
 
 //		val client = MinecraftClient.getInstance()
